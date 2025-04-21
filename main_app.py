3f33ca92f185119179a93b84fdc65e93beaca482
muhannad <Almuhannad@OmantelAcademy.com>
2025-04-21 13:52:13 +0400

    bitwise function is now working

--- main_app.py
+++ main_app.py
@@ -16,3 +16,23 @@ print(num_1)
 print(num_2)
 
 print("result is: ", result)
+
+def absolute_function():
+    m = int(input("Enter a number to Check: "))
+    print(abs(m))
+absolute_function()
+
+def betwise_function():
+    a = (int(input("Enter first number: ")))
+    b = (int(input("Enter secound number: ")))
+    select_now  = input("SELECT & or | or ^")
+
+    if select_now == "&":
+        print(a & b)
+    elif select_now == "|":
+        print(a | b)
+    elif select_now =="^":
+        print(a ^ b)
+
+betwise_function()
+        
