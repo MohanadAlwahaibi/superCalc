8e5082eb3f3ca9e839ce1ee08b20db5c116404cd
muhannad <Almuhannad@OmantelAcademy.com>
2025-04-21 09:11:16 +0400

    Also the operation + -  are working correctly

--- main_app.py
+++ main_app.py
@@ -1,5 +1,14 @@
-num_1 = input("Enter first number : ")
-num_2 = input("Enter secound number: ")
+num_1 = int(input("Enter first number : "))
+num_2 = int(input("Enter secound number: "))
+opp = input("Select + or -")
+result = 0
+
+if opp == "+":
+    result = num_1 + num_2
+elif    opp == "-":
+    result = num_1 - num_2
 
 print(num_1)
 print(num_2)
+
+print("result is: ", result)
