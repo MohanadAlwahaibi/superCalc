1dbaeae6d38733dc37f6eed3fa0d11511b75ea71
muhannad <Almuhannad@OmantelAcademy.com>
2025-04-21 09:13:16 +0400

    also the multipliction function wokring fine

--- main_app.py
+++ main_app.py
@@ -1,12 +1,14 @@
 num_1 = int(input("Enter first number : "))
 num_2 = int(input("Enter secound number: "))
-opp = input("Select + or -")
+opp = input("Select + or - or *: ")
 result = 0
 
 if opp == "+":
     result = num_1 + num_2
 elif    opp == "-":
     result = num_1 - num_2
+elif    opp == "*":
+    result = num_1 * num_2
 
 print(num_1)
 print(num_2)
