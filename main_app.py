55460ec11256f911e0ebebfadcd19255867d78a0
muhannad <Almuhannad@OmantelAcademy.com>
2025-04-21 09:31:37 +0400

    the devision operation is working fine

--- main_app.py
+++ main_app.py
@@ -1,6 +1,6 @@
 num_1 = int(input("Enter first number : "))
 num_2 = int(input("Enter secound number: "))
-opp = input("Select + or - or *: ")
+opp = input("Select + or - or * or /: ")
 result = 0
 
 if opp == "+":
@@ -9,6 +9,8 @@ elif    opp == "-":
     result = num_1 - num_2
 elif    opp == "*":
     result = num_1 * num_2
+elif    opp == "/":
+    result = num_1 // num_2
 
 print(num_1)
 print(num_2)
