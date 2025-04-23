dacfa73c14e18176f0c736c4a77064f42fb23974
NABIL-ENG1 <Nabeel4khalid@gmail.com>
2025-04-23 11:03:09 +0400

    work is done

--- practice_nabil.py
+++ practice_nabil.py
@@ -1,6 +1,6 @@
 num_1 = int(input("Enter a number: "))
 num_2 = int(input("Enter a secound number: "))
-operation = input("SELECT + OR - OR % : ")
+operation = input("SELECT + OR - OR % OR * : ")
 result = 0
 
 if operation == "+":
@@ -8,4 +8,7 @@ if operation == "+":
     print(result)
 elif operation == "-":
     result = num_1 - num_2
+    print(result)
+elif operation == "*":
+    result = num_1 * num_2
     print(result)
